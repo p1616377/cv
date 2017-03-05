eb89f01bc6636684726b6e3503369887d558e783
p1616377 <jean-de-matha.tsang-manga@etu.univ-lyon1.fr>
2017-03-05 21:05:21 +0100

    Update validerFormulaire.js

--- validerFormulaire.js
+++ validerFormulaire.js
@@ -1,8 +1,8 @@
 function valider()
  {
-	var Nom = $("#nom").val();
-	if (Nom == "")
+	console.log($("#nom").val());
+	if ($("#nom").val() == "")
      	{
-         window.alert ("La rubrique réservéé au NOM est vide, Veuillez saisir votre Nom !");
+         window.alert("La rubrique réservéé au NOM est vide, Veuillez saisir votre Nom !");
 	}
  } 
