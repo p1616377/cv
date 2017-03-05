af0c47fd186325bb6b3c5324462efa00c7dc8f8d
p1616377 <jean-de-matha.tsang-manga@etu.univ-lyon1.fr>
2017-03-05 17:43:44 +0100

    Update cv.js

--- cv.js
+++ cv.js
@@ -1,1 +1,13 @@
 function afficher(id_div) {$('#'+id_div).toggle();}
+
+function valider() 
+{    
+  var Nom = $(nom).val();
+  
+  if ( Nom == "" )     
+  {
+   alert ( "Veuillez entrer votre Nom !" );         
+   valid = false; 
+   return valid;     
+  } 
+}
