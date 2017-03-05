6d15b85d3938a4fdf92c3a6c75e05a57920bb386
p1616377 <jean-de-matha.tsang-manga@etu.univ-lyon1.fr>
2017-03-05 21:00:29 +0100

    Update validerFormulaire.js

--- validerFormulaire.js
+++ validerFormulaire.js
@@ -1,9 +1,6 @@
 function valider()
  {
 	var Nom = $("#nom").val();
-	var Courriel = $("#courriel").val();
-	var Telephone = $("#telephone").val();
-
 	if ( Nom == "" )
      	{
          window.alert ( "La rubrique réservéé au NOM est vide, Veuillez saisir votre Nom !" );
