8772c4cc2c9acb48ab32819b231dccd0c4aab7ef
p1616377 <jean-de-matha.tsang-manga@etu.univ-lyon1.fr>
2017-03-05 18:24:47 +0100

    Add files via upload

--- cv.js
+++ cv.js
@@ -1,17 +1,4 @@
 function afficher(id_div) {$('#'+id_div).toggle();}
 
-function valider() 
-{    
-  var Nom = $(Nom).val();
-  
-  if ( Nom == "" )     
-  {
-   alert ( "Veuillez entrer votre Nom !" );         
-   valid = false; 
-  }
-  else 
-  {valid = true;}
-  return valid;     
-  } 
-  
-}
+
+
