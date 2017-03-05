3a2b0cfda301adfbb016a07df1411fd4b4f6fde9
p1616377 <jean-de-matha.tsang-manga@etu.univ-lyon1.fr>
2017-03-05 18:28:09 +0100

    Update validerFormulaire.js

--- validerFormulaire.js
+++ validerFormulaire.js
@@ -1,8 +1,6 @@
 function valider()
  {
     var n = document.formulaire.nom.value;
-	var Courriel = $(courriel).val();
-	var Téléphone = $(telephone).val();
 
 	if ( n == "" )
      {
@@ -12,25 +10,4 @@ function valider()
 	else {valid = true;}
 	return valid;
      } 
-   
-
-    if ( document.formulaire.Courriel.value == "" )
-     {
-         alert ( "Veuillez entrer votre courriel !" );
-         valid = false;
- return valid;
-     }
-
-    if ( document.formulaire.Telephone.value == "" )
-     {
-         alert ( "Veuillez entrer votre numéro de téléphone !" );
-         valid = false;
- return valid;
-     }
-     if ( document.formulaire.Adresse.value == "" )
-     {
-         alert ( "Veuillez entrer votre adresse !" );
-         valid = false;
- return valid;
-     }
- }
+}
