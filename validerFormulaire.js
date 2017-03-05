a370a5d62b8f6cdb6b516df316dfd52f8eb55c0f
p1616377 <jean-de-matha.tsang-manga@etu.univ-lyon1.fr>
2017-03-05 21:50:11 +0100

    Add files via upload

--- validerFormulaire.js
+++ validerFormulaire.js
@@ -1,10 +1,38 @@
-function valider()
+function validation()
  {
+
 	console.log($("#nom").val());
 	if ($("#nom").val() == "")
      	{
          window.alert("La rubrique réservée au NOM est vide, Veuillez saisir votre Nom !");
-	 
-	}
+		}
+
+	console.log($("#courriel").val());
+	console.log($("#courriel").val().includes("@"));
+	if ($("#courriel").val() == "")
+		{
+         window.alert("La rubrique réservée au COURRIEL est vide, Veuillez saisir votre Courriel !");
+		}
+	else
+		{
+		if (!$("#courriel").val().includes("@"))
+			{
+			 window.alert("Le Courriel saisie n'est pas conforme, Veuillez saisir une bon Courriel !");
+			}
+		}
 	
- } 
+		
+	console.log($("#telephone").val());
+	console.log($("#telephone").val().length);
+	if ($("#courriel").val() == "")
+		{
+         window.alert("La rubrique réservée au NUMERO DE TELEPHONE est vide, Veuillez saisir votre Numero de Téléphone !");
+		}
+	else
+		{
+		if (!$("#courriel").val().length != 10)
+			{
+			 window.alert("Le Numéro de téléphone saisie est incorrect, Veuillez saisir un Numéro de Téléphone correct !");
+			}
+		}
+ }
